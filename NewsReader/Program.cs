1ea53b5b6e107916497bdeb582a5c7eafdfdf621
Simon <sbridge67@gmail.com>
2018-10-26 11:38:31 +0100

    project creation

---
+++ NewsReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewsReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
